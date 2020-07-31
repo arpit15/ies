ff8e45fb68171b502ba065b5df21e1370e250fba
arpit agarwal <arpit15945@gmail.com>
2020-07-31 15:33:31 -0400

    removed path parsing and works on mac

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "ies_Loader.h"
 #include "rgbe.h"
 
-#include <io.h>
+// #include <io.h>
 #include <fstream>
 #include <iostream>
 
@@ -65,24 +65,27 @@ bool IES2HDR(const std::string& path, const std::string& outpath, IESFileInfo& i
 
 bool IES2HDR(const std::string& path, IESFileInfo& info)
 {
-	char drive[_MAX_DRIVE];
-	char dir[_MAX_DIR];
-	char fname[_MAX_FNAME];
-	char ext[_MAX_EXT];
+	// char drive[_MAX_DRIVE];
+	// char dir[_MAX_DIR];
+	// char fname[_MAX_FNAME];
+	// char ext[_MAX_EXT];
 
-	std::memset(drive, 0, sizeof(drive));
-	std::memset(dir, 0, sizeof(dir));
-	std::memset(fname, 0, sizeof(fname));
-	std::memset(ext, 0, sizeof(ext));
+	// std::memset(drive, 0, sizeof(drive));
+	// std::memset(dir, 0, sizeof(dir));
+	// std::memset(fname, 0, sizeof(fname));
+	// std::memset(ext, 0, sizeof(ext));
 
-	_splitpath(path.c_str(), drive, dir, fname, ext);
+	// _splitpath(path.c_str(), drive, dir, fname, ext);
 
-	std::string out = drive;
-	out += dir;
-	out += fname;
-	out += ".hdr";
+	// std::string out = drive;
+	// out += dir;
+	// out += fname;
+	// out += ".hdr";
 
-	return IES2HDR(path, out, info);
+	std::string infile = path + ".ies";
+	std::string out = path + ".hdr";
+
+	return IES2HDR(infile, out, info);
 }
 
 int main(int argc, const char* argv[])
@@ -92,69 +95,70 @@ int main(int argc, const char* argv[])
 		if (argc <= 1)
 		{
 			std::cout << "IES2HDR Info: ERROR! Please enter a path to a IES file/directory." << std::endl;
-			std::system("pause");
+			// std::system("pause");
 			return EXIT_FAILURE;
 		}
 
-		char drive[_MAX_DRIVE];
-		char dir[_MAX_DIR];
-		char fname[_MAX_FNAME];
-		char ext[_MAX_EXT];
+		// char drive[_MAX_DRIVE];
+		// char dir[_MAX_DIR];
+		// char fname[_MAX_FNAME];
+		// char ext[_MAX_EXT];
 
-		std::memset(drive, 0, sizeof(drive));
-		std::memset(dir, 0, sizeof(dir));
-		std::memset(fname, 0, sizeof(fname));
-		std::memset(ext, 0, sizeof(ext));
+		// std::memset(drive, 0, sizeof(drive));
+		// std::memset(dir, 0, sizeof(dir));
+		// std::memset(fname, 0, sizeof(fname));
+		// std::memset(ext, 0, sizeof(ext));
 
-		_splitpath(argv[1], drive, dir, fname, ext);
+		// _splitpath(argv[1], drive, dir, fname, ext);
 
-		if (_stricmp(ext, ".ies") == 0)
+		// if (_stricmp(ext, ".ies") == 0)
+		if(1)
 		{
 			IESFileInfo info;
 			if (!IES2HDR(argv[1], info))
 				std::cout << "IES2HDR Error: " << info.error() << std::endl;
 		}
-		else
-		{
-			std::string path = drive;
-			path += dir;
-			path += fname;
-			path += "/*.ies";
+		// else
+		// {
+		// 	std::string path = drive;
+		// 	path += dir;
+		// 	path += fname;
+		// 	path += "/*.ies";
 
-			_finddata_t fileinfo;
-			std::memset(&fileinfo, 0, sizeof(fileinfo));
+		// 	_finddata_t fileinfo;
+		// 	std::memset(&fileinfo, 0, sizeof(fileinfo));
 
-			auto handle = _findfirst(path.data(), &fileinfo);
-			if (handle != -1)
-			{
-				do
-				{
-					if (!(fileinfo.attrib & _A_SUBDIR))
-					{
-						std::string filename;
-						filename = drive;
-						filename += dir;
-						filename += fname;
-						filename += "/";
-						filename += fileinfo.name;
+		// 	auto handle = _findfirst(path.data(), &fileinfo);
+		// 	if (handle != -1)
+		// 	{
+		// 		do
+		// 		{
+		// 			if (!(fileinfo.attrib & _A_SUBDIR))
+		// 			{
+		// 				std::string filename;
+		// 				filename = drive;
+		// 				filename += dir;
+		// 				filename += fname;
+		// 				filename += "/";
+		// 				filename += fileinfo.name;
 
-						std::cout << "IES2HDR Info: Converting IES to HDR :" << filename << std::endl;
+		// 				std::cout << "IES2HDR Info: Converting IES to HDR :" << filename << std::endl;
 
-						IESFileInfo info;
-						if (!IES2HDR(filename, info))
-							std::cout << "IES2HDR Error: " << info.error() << std::endl;
-					}
-				} while (_findnext(handle, &fileinfo) == 0);
+		// 				IESFileInfo info;
+		// 				if (!IES2HDR(filename, info))
+		// 					std::cout << "IES2HDR Error: " << info.error() << std::endl;
+		// 			}
+		// 		} while (_findnext(handle, &fileinfo) == 0);
 
-				_findclose(handle);
-			}
-		}
+		// 		_findclose(handle);
+		// 	}
+		// }
 	}
 	catch (const std::exception& e)
 	{
 		std::cerr << e.what() << std::endl;
 	}
 
-	std::system("pause");
+	// std::system("pause");
 	return 0;
 }
